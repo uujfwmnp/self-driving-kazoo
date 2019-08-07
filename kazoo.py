0160f5a9e396b1595c577d585adbf2f880d9ad76
uujfwmnp <3219994+uujfwmnp@users.noreply.github.com>
2019-08-07 01:23:05 -0700

    Update kazoo.py
    I need to change this later, the while loop is all wrong. Likely blow this up into three separate functions, one for starting the music during a caution, one for stopping the music when "one to green" is seen, and one to toggle between the two.
    
    However, it at least does play music.

--- kazoo.py
+++ kazoo.py
@@ -4,14 +4,13 @@ import time
 from pygame import mixer
 from irsdk import Flags
 
-musicLocation = 'F:/iRacing/Local Forecast - Elevator.mp3'
+musicLocation = 'Local Forecast - Elevator.mp3'
 
 ir = irsdk.IRSDK()
 ir.startup()
-# Testing: Comment the above two lines, uncomment the two below
+# To test: Comment out the above two lines, uncomment the two below
 #ir = {}
 #sess_flag = ir['SessionFlags'] = 268452352
-pygame.init() # Initialize pygame
 mixer.init()  # Initialize pygame mixer
 
 def flagSet(flagType):
@@ -22,19 +21,25 @@ def cautionMusic():
     musicStatus = "" #set this outside
     try:
         while True:
-            if (flagSet(Flags.caution_waving) == True):
+            if (flagSet(Flags.caution) == True):
                 print('Caution Is Waving, Playing Music')
-                if (musicStatus = Playing): # If the music is already playing...
+                if (musicStatus == 'Playing'): # If the music is already playing...
                     time.sleep(1) # Do nothing
                 else:
-                    musicStatus = Playing # Set the variable
+                    musicStatus = 'Playing' # Set the variable
                     mixer.music.load(musicLocation) # Load the song
                     mixer.music.play(loops=-1) # Play it infinitely
+                    while mixer.music.get_busy():
+                        pygame.time.delay(100)
             if (flagSet(Flags.one_lap_to_green) == True):
                 print('One Lap To Green, Stopping Music')
-                musicStatus = "" # Set the variable back to empty
-                mixer.fadeout(5000) # Five second long fadeout, then music stops
+                if (musicStatus == 'Stopped'): # If the music is already playing...
+                    time.sleep(1) # Do nothing
+                else:
+                    musicStatus = 'Stopped' # Set the variable to
+                    mixer.fadeout(5000) # Five second long fadeout, then music stops
             else:
+                print('Not under caution, sleeping')
                 time.sleep(5) # Wait 5 seconds before checking session flags again
     except KeyboardInterrupt:
         print('Ending Program\n')
