af5fadb35b2ae532296d5c00a7982c243ff1a79d
uujfwmnp <3219994+uujfwmnp@users.noreply.github.com>
2019-09-08 05:30:29 -0700

    I unbroke it with a break!
    Who knew that while loops need to break out?
    * Added additional test segments for changing flag status via a text file
    * Added a break to the while loop, to stop "mixer not initialized" errors
    * Removed useless if/else in the stopPlayback function
    * Stopped calling the stopPlayback function from within the flagStatus function
    ** This also stops chances of  "mixer not initialized" errors

--- kazoo.py
+++ kazoo.py
@@ -4,8 +4,9 @@ from pygame import mixer
 from irsdk import Flags
 from mutagen.mp3 import MP3
 
-test = True # Set to True for test mode, False for live
 playlistLocation = 'C:/path/to/playlist.m3u' # Yeah, you need to convert \ to / for Windows, sorry.
+test = False # Set to True for test mode, False for live
+testFile = 'C:/path/to/flag-status.txt' # Text file for a flag state number: 268451840=caution 268452352=one_lap_to_green 268435456=green
 
 m3u = m3u8.load(playlistLocation)
 musicLocation = m3u.segments.uri
@@ -24,29 +25,29 @@ def cautionMusic(sessionFlag):
     mixer.music.play() # Play!
     while mixer.music.get_busy():
         pygame.time.delay(100) # This prevents the chosen song from stopping too soon.
-        sessionFlag = ir['SessionFlags'] # This should hopefully update from live data
+        if test == True:
+            sessionFlag = ir['SessionFlags'] = int(open(testFile, 'r').read())
+        else:
+            sessionFlag = ir['SessionFlags'] # This should hopefully update from live data
         if (sessionFlag & Flags.one_lap_to_green):
             stopPlayback()
-    mixer.quit() # Unload the mixer
+            break # End the while loop
 
 def stopPlayback():
     print('One Lap To Green, Stopping Music')
-    if (mixer.music.get_busy() == True):
-        mixer.music.fadeout(5000) # Five second long fadeout, then music stops
-        time.sleep(6) # This keeps the function from being repeatedly called during the fadeout
-        mixer.quit() # Unload the mixer
-    else:
-        print('Music is stopped')
-        time.sleep(10) # This keeps the function from being repeatedly called during the fadeout
+    mixer.music.fadeout(5000) # Five second long fadeout, then music stops
+    time.sleep(6) # This keeps the function from being repeatedly called during the fadeout
+    mixer.quit() # Unloads the mixer, required to re-initialize with new sample rates
 
 def flagStatus(sessionFlag):
     if (sessionFlag & Flags.one_lap_to_green): # I don't think I need this anymore, since the cautionMusic function handles it all.
-        stopPlayback()
+        print('Music is stopped')
+        time.sleep(10)
     elif (sessionFlag & Flags.caution) or (sessionFlag & Flags.caution_waving): # If the sessionFlag decimal contains the Caution flag in hex, call the function below
         cautionMusic(sessionFlag)
     else:
         print('Not under caution, sleeping')
-        time.sleep(10) # Wait 5 seconds before checking session flags again
+        time.sleep(10) # Wait 10 seconds before checking session flags again
 
 if test == True:
     print('\n -------------------------------------------------------------')
@@ -57,7 +58,7 @@ try:
     while True:
         if test == True:
             ir = {}
-            sessionFlag = ir['SessionFlags'] = 268451840 # 268451840=caution 268452352=one_lap_to_green
+            sessionFlag = ir['SessionFlags'] = int(open(testFile, 'r').read())
             flagStatus(sessionFlag)
             time.sleep(1)
         else:
@@ -72,6 +73,6 @@ try:
                 flagStatus(sessionFlag)
                 time.sleep(1)
 except KeyboardInterrupt:
-    print("Ending Program\n")
+    print('Ending Program\n')
     quit()
 pass
